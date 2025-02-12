c924069adf4609dfc307f8265ada7864ba6ac9bc
BuzzLightyear-01 <mslegaspi25@gmail.com>
2025-02-13 01:41:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -22,10 +22,10 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
+            text: "Love mo ba ko? 🥺👉🏻👈🏻",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            secretAnswer: "Super duper love hehe! ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
