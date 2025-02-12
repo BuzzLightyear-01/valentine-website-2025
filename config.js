beacd81c9ba2f9f8f0f884bcdacfe720a294277e
BuzzLightyear-01 <mslegaspi25@gmail.com>
2025-02-13 01:50:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -49,9 +49,9 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        title: "hehehe sobrang swerte ko sayo mwa! 😙",
+        message: "kiss mo nga mo and wag ka na mag tampurorot🙄!",
+        emojis: "😙😗😘💋🫶🏻❤️"  // These will bounce around
     },
 
     // Color scheme for the website
